a657b2397531021f13663efd79f7893e41ab808b
LdKhanh <khanhleemtp>
2020-08-26 16:53:25 +0700

    11-times

---
+++ chapter-11/sandbox.js
@@ -0,0 +1,53 @@
+
+// dates & times
+
+// const now = new Date();
+
+// console.log(now);
+// console.log(typeof now);
+// console.log('getFullYear: ', now.getFullYear());
+// console.log('getMonth: ', now.getMonth());
+// position in month, begin: 1 
+// console.log('getDate: ', now.getDate());
+
+// position in day, begin: 1 
+// console.log('getDay: ', now.getDay());
+// console.log('getMinutes: ', now.getMinutes());
+// console.log('getSeconds: ', now.getSeconds());
+
+// timestamps: begin an january 1970
+// console.log('timestamps', now.getTime());
+
+// date strings
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleString());
+
+// ---------------------------------------------------------
+// timestamps
+
+// const before = new Date('August 26 2020 15:16:54')
+// const now = new Date();
+
+// miliseconds
+// console.log(now.toDateString());
+
+// console.log(now.getTime(), before.getTime());
+
+// const diff = now.getTime() - before.getTime();
+
+// const min = Math.round(diff/1000/60);
+// const hour = Math.round(min/60);
+// const day = Math.round(hour/24);
+
+// console.log(`This blog was created at ${min} minutes ago`);
+
+// convert
+
+// console.log(now.getTime());
+
+// const timestamps = new Date(1598430325667);
+// console.log(timestamps)
+
+
+// ======================date-fns=======================
